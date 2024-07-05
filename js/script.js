2b0d8f099bd1058ea3a765b8d306c3427fe2de8e
MattiaPiazzolla <mattiapiazzolla@gmail.com>
2024-07-05 16:38:23 +0200

    [UPD] defined the grid destination

--- js/script.js
+++ js/script.js
@@ -1,2 +1,4 @@
 // RECUPERO IL PULSANTE PER INIZIARE IL GIOCO
 const playBtn = document.getElementById('playBtn');
+// DEFINISCO IL CONTENITORE DELLA GRIGLIA
+const gridContainer = document.getElementById('gridContainer');
